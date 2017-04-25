559c2e47494b32d423c59a121881d714bf2e0e35
zamgi <denisov72@mail.ru>
2017-04-26 02:49:50 +0300

    Update --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++ --clear-after-build--.cmd
@@ -2,6 +2,7 @@ del "*.suo" /S/Q/F/A
 del "*.csproj.user" /S/Q/F/A
 del "ner_ru.web.demo\bin\*.pdb" /Q
 rd "ner_ru.web.demo\obj" /S/Q
+rd "ner_ru.web.demo\(logs)" /S/Q
 rd "captcha\bin" /S/Q
 rd "captcha\obj" /S/Q
 rd "crfsuite.net\bin" /S/Q
