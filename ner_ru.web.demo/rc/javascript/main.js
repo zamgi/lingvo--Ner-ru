fdcfa95dd6aef8e53bb89666aa02948bedd51ddc
zamgi <zamgi@gmail.com>
2017-04-26 23:34:29 +0300

    ...

--- ner_ru.web.demo/rc/javascript/main.js
+++ ner_ru.web.demo/rc/javascript/main.js
@@ -119,13 +119,12 @@ $(document).ready(function () {
         
     });
 
-    force_load_model();
-
     function processing_start(){
         $('#text').addClass('no-change').attr('readonly', 'readonly').attr('disabled', 'disabled');
-        $('.result-info').show().removeClass('error').text('Идет обработка...');
+        $('.result-info').show().removeClass('error').html('Идет обработка... <label id="processingTickLabel"></label>');
         $('#processButton').addClass('disabled');
         $('#processResult tbody').empty();
+        setTimeout(processing_tick, 1000);
     };
     function processing_end(){
         $('#text').removeClass('no-change').removeAttr('readonly').removeAttr('disabled');
@@ -138,13 +137,13 @@ $(document).ready(function () {
     function is_text_empty(text) {
         return (text.replace(/(^\s+)|(\s+$)/g, "") == "");
     };
-    function force_load_model() {
+    (function() {
         $.ajax({
             type: "POST",
             url: "RESTProcessHandler.ashx",
             data: { splitBySmiles: true, html: false, text: "_dummy_" }
         });
-    };
+    })();
 
     String.prototype.insert = function (index, str) {
         if (0 < index)
@@ -178,5 +177,23 @@ $(document).ready(function () {
         $("span.O").attr("title", "O  - !!!!!!!");
         $("span.__UNDEFINED__").attr("title", "__UNDEFINED__ - !!!!!!!");
         $("span.__UNKNOWN__").attr("title", "__UNKNOWN__ - !!!!!!!!");
+    };
+
+    var processingTickCount = 1;
+    function processing_tick() {
+        var n2 = function (n) {
+            n = n.toString();
+            return ((n.length == 1) ? ('0' + n) : n);
+        }
+        var d = new Date(new Date(new Date(new Date().setHours(0)).setMinutes(0)).setSeconds(processingTickCount));
+        var t = n2(d.getHours()) + ':' + n2(d.getMinutes()) + ':' + n2(d.getSeconds()); //d.toLocaleTimeString();
+        var $s = $('#processingTickLabel');
+        if ($s.length) {
+            $s.text(t);
+            processingTickCount++;
+            setTimeout(processing_tick, 1000);
+        } else {
+            processingTickCount = 1;
+        }
     };
 });
